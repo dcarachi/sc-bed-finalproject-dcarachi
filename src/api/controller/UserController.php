72cafae28e029ebdda810e27bf2c27e7c74d8705
David Christopher Carachi <david.carachi@outlook.com>
2024-10-30 10:33:22 +0100

    Added ability to retreive logged in user info, and refactored register method.

--- src/api/controller/UserController.php
+++ src/api/controller/UserController.php
@@ -8,40 +8,63 @@ class UserController extends Controller
 {
     public static function register(array $params, array $data): void
     {
-        $requiredData = ['email', 'password', 'firstName', 'lastName', 'accessLevel'];
-        if (self::checkFieldsSet($data, $requiredData)) {
-            $email = $data['email'];
-            $password = $data['password'];
-            $firstName = $data['firstName'];
-            $lastName = $data['lastName'];
-            $accessLevel = AccessLevel::tryFrom($data['accessLevel']);
-
-            if ($accessLevel) {
-                if (User::isEmailAvailable($email)) {
-                    $user = new User($email, $password, $accessLevel, $firstName, $lastName);
-                    $user = User::save($user);
-                    if ($user) {
-                        self::sendResponse(['user' => $user]);
-                    } else {
-                        self::sendResponse(code: 500, response: ['error' => 'Failed to create new user.']);
-                    }
-                } else {
-                    self::sendResponse(code: 400, response: ['error' => 'Email address specified is not available.']);
-                }
-            } else {
-                $response['error'] = 'Invalid access level specified';
-                $response['allowedValues'] = AccessLevel::cases();
-                self::sendResponse(code: 400, response: $response);
+        // Check if the required input fields are present.
+        $requiredFields = ['email', 'password', 'firstName', 'lastName', 'accessLevel'];
+        foreach ($requiredFields as $field) {
+            if (!isset($data[$field])) {
+                self::sendResponse(
+                    code: 400,
+                    error: [
+                        'message' => 'One or more fields are missing.',
+                        'requiredFields' => $requiredFields
+                    ]
+                );
+                return;
             }
-        } else {
-            $response['error'] = 'Invalid request. One or more fields are missing.';
-            $response['requiredFields'] = $requiredData;
-            self::sendResponse(code: 400, response: $response);
         }
+        // Ensure email address is unique.
+        $email = $data['email'];
+        $password = $data['password'];
+        if (!User::isEmailAvailable($email)) {
+            self::sendResponse(code: 400, error: 'Email address given is not available.');
+            return;
+        }
+        // Ensure access level values are legal.
+        $accessLevel = AccessLevel::tryFrom($data['accessLevel']);
+        if (!$accessLevel) {
+            self::sendResponse(
+                code: 400,
+                error: [
+                    'message' => 'Invalid access level specified.',
+                    'allowedValues' => AccessLevel::cases()
+                ]
+            );
+            return;
+        }
+        // Attempt registration.
+        $firstName = $data['firstName'];
+        $lastName = $data['lastName'];
+        $user = new User($email, $password, $accessLevel, $firstName, $lastName);
+        $user = User::save($user);
+        if (!$user) {
+            self::sendResponse(code: 500, error: 'Create new user failed.');
+            return;
+        }
+        self::sendResponse($user);
     }
 
-    public static function getInfo(array $request, array $token): void
+    public static function getInfo(array $request, array $data): void
     {
-        self::sendResponse(code: 501, response: ['error' => 'Method not yet implemented.']);
+        if (!self::checkToken($data)) {
+            self::sendResponse(code: 401, error: 'Missing, invalid, or expired token.');
+            return;
+        }
+        $user = new User(id: $data['api_user']);
+        $user = User::load($user);
+        if (!$user) {
+            self::sendResponse(code: 500, error: 'Loading user data failed.');
+            return;
+        }
+        self::sendResponse($user);
     }
 }
